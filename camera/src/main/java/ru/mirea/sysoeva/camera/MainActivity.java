3089cacbaab5718ee4c8e1c39b71b6e926b64e33
Anna Sysoeva <curlyannas0@gmail.com>
2022-04-28 21:54:44 +0300

    починила камеру

--- camera/src/main/java/ru/mirea/sysoeva/camera/MainActivity.java
+++ camera/src/main/java/ru/mirea/sysoeva/camera/MainActivity.java
@@ -1,11 +1,5 @@
 package ru.mirea.sysoeva.camera;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,8 +7,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,56 +30,55 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 0;
     private boolean isWork = false;
     private Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        // Выполняется проверка на наличие разрешений на использование камеры и запись в память
+        imageView = (ImageView) findViewById(R.id.imageView);
         int cameraPermissionStatus =
                 ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
+
+        int storagePermissionStatus = ContextCompat
+                .checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED
+                && storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
             isWork = true;
         } else {
-            // Выполняется запрос к пользователь на получение необходимых разрешений
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_CODE_PERMISSION_CAMERA);
         }
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Если приложение камера вернула RESULT_OK, то производится установка изображению imageView
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
             imageView.setImageURI(imageUri);
         }
     }
-    public void imageViewOnClick(View view) {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // проверка на наличие разрешений для камеры
-        if (cameraIntent.resolveActivity(getPackageManager()) != null && isWork == true)
-        {
+
+    public void onClick(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Log.d("1", "Nothing");
+        if(isWork){
+            Log.d("1", "Nothing2");
             File photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            // генерирование пути к файлу на основе authorities
+
             String authorities = getApplicationContext().getPackageName() + ".fileprovider";
             imageUri = FileProvider.getUriForFile(this, authorities, photoFile);
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, CAMERA_REQUEST);
         }
     }
-    /**
-     * Производится генерирование имени файла на основе текущего времени и создание файла
-     * в директории Pictures на ExternelStorage.
-     * class.
-     * @return File возвращается объект File .
-     * @exception IOException если возвращается ошибка записи в файл
-     */
+
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "IMAGE_" + timeStamp + "_";
@@ -85,13 +86,13 @@ public class MainActivity extends AppCompatActivity {
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         return File.createTempFile(imageFileName, ".jpg", storageDirectory);
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-// производится проверка полученного результата от пользователя на запрос разрешения Camera
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_PERMISSION_CAMERA) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // permission granted
                 isWork = true;
             } else {
                 isWork = false;
